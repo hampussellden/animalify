fe5cc3a383c18176e44ab10d9b702e1fc1bcda69
hampussellden <hampussellden1@gmail.com>
2023-05-25 15:53:53 +0200

    fix readability in openAI component

--- src/components/OpenAI/OpenAI.tsx
+++ src/components/OpenAI/OpenAI.tsx
@@ -1,22 +1,26 @@
 
 import { Animal } from '../../types';
 import { useQuery } from '@tanstack/react-query';
+import { useState } from 'react';
 
 function OpenAI(props:{animal:Animal}) {
+
   const animal = props.animal;
+  
+  const body = JSON.stringify({animal});
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: body
+  }
   const { isLoading, error, data } = useQuery({
-    queryKey: ['repoData'],
+    queryKey: ['data'],
     queryFn: () =>
-      fetch('https://localhost:3000',{
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({animal})
-      }).then(
-        (res) => res.json(),
-      ),
-  })
+    fetch('https://localhost:3000/api',options).then(
+      (res) => res.json())
+    });
 
   if (isLoading) return(
     <div>
@@ -29,10 +33,13 @@ function OpenAI(props:{animal:Animal}) {
       <h1>Error</h1>
     </div>
   )
-
+  if (data){
+    console.log(data);
+  }
   return (
     <div>
-      <h1>{data}</h1>
+      {/* <h1>{data}</h1> */}
+      <h2>hello world</h2>
     </div>
   )
 }
