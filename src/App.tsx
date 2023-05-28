b31345b38e9185adeabdd3b3c7ff53332607664b
tpku <tpku@icloud.com>
2023-05-28 23:00:00 +0200

    Structure, styling and merge of all commits
    App.css: styling added and changed.
    Hero section is temporary hardcoded.
    
    App.tsx: Added components Header, AnimalSelectList and AnimalListItem.
    Added temporary hardcoded hero section.
    Added sections.
    Uncommented unused components Card and AnimalSelect.
    
    Index.css: Added and removed global styling.

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,62 @@
-import { useEffect, useState } from 'react'
-import { mockAnimals } from './assets/animals'
-import './App.css'
-import AnimalSelect from './components/AnimalSelect';
-import Card from './components/Card';
-import CreateNewAnimal from './components/CreateNewAnimal';
-import OpenAI from './components/OpenAI';
-import { Animal } from './types';
-import {
-  QueryClient,
-  QueryClientProvider,
-  useQuery,
-} from '@tanstack/react-query'
+import { useEffect, useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import "./App.css";
 
-const queryClient = new QueryClient()
+import { mockAnimals } from "./assets/animals";
+import { Animal } from "./types";
+
+// import AnimalSelect from "./components/AnimalSelect";
+// import Card from "./components/Card";
+import Header from "./components/Header";
+import AnimalMutation from "./components/AnimalMutation";
+import AnimalSelectList from "./components/AnimalSelectList";
+import AnimalListItem from "./components/AnimalListItem";
+
+// import OpenAI from './components/OpenAI';
+
+const queryClient = new QueryClient();
 function App() {
-  const [animalOne, setAnimalOne] = useState<Animal>();
-  const [animalTwo, setAnimalTwo] = useState<Animal>();
+    const [animalOne, setAnimalOne] = useState<Animal>();
+    const [animalTwo, setAnimalTwo] = useState<Animal>();
 
-  useEffect(() => {
-    console.log({animalOne})
-    console.log({animalTwo})
-  }, [animalOne, animalTwo]);
+    useEffect(() => {
+        console.log(animalOne?.name);
+        console.log(animalTwo?.name);
+    }, [animalOne, animalTwo]);
 
-  return (
-    <QueryClientProvider client={queryClient}>
-      <nav>
-        <AnimalSelect selectName="animalOne-drop-down" animals={mockAnimals} onChange={setAnimalOne} />
-        <AnimalSelect selectName="animalTwo-drop-down" animals={mockAnimals} onChange={setAnimalTwo} />
-      </nav>
-        <main>
-          { animalOne && <Card animal={animalOne} />}
-          { animalTwo && <Card animal={animalTwo} />}
-        </main>
-        <section>
-          { animalOne && animalTwo ? <CreateNewAnimal selectedAnimal1={animalOne} selectedAnimal2={animalTwo}/> : <></>}
-        </section>
-      <section>
-        {/* {animalOne && <OpenAI animal={animalOne}/>} */}
-      </section>
-    </QueryClientProvider>
-  )
+    return (
+        <QueryClientProvider client={queryClient}>
+            <Header />
+            <main>
+                <section className="hero">
+                    <div className="content-wrapper">
+                        <span>
+                            A <p>IMALIFY</p>
+                        </span>
+                        <h2>CREATE YOUR OWN DREAM ANIMAL USING A SELECTION OF ANIMALS FROM OUR DATABASE.</h2>
+                        <p>Select two different creatures and let the doctor work her magic to uncover the mutation we get</p>
+                    </div>
+                </section>
+                <section className="animal-selection">
+                    <AnimalSelectList>
+                        <AnimalListItem onClick={setAnimalOne} animals={mockAnimals} handleSelected={animalOne?.name} />
+                    </AnimalSelectList>
+                    <AnimalSelectList>
+                        <AnimalListItem onClick={setAnimalTwo} animals={mockAnimals} handleSelected={animalTwo?.name} />
+                    </AnimalSelectList>
+                </section>
+
+                <section>{animalOne && animalTwo ? <AnimalMutation selectedAnimal1={animalOne} selectedAnimal2={animalTwo} /> : <></>}</section>
+
+                {/* <AnimalSelect selectName="animalOne-drop-down" animals={mockAnimals} onChange={setAnimalOne} /> */}
+                {/* <AnimalSelect selectName="animalTwo-drop-down" animals={mockAnimals} onChange={setAnimalTwo} /> */}
+                {/* {animalOne && <Card animal={animalOne} />}
+                {animalTwo && <Card animal={animalTwo} />} */}
+            </main>
+
+            <section>{/* {animalOne && <OpenAI animal={animalOne}/>} */}</section>
+        </QueryClientProvider>
+    );
 }
 
-export default App
+export default App;
